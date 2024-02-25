b9ea900fc5f47c5522603e15103992d4b1f3499c
Nino12qeshelashvili <156595427+Nino12qeshelashvili@users.noreply.github.com>
2024-02-25 02:06:17 -0800

    at home 3

---
+++ at home.py
@@ -0,0 +1,8 @@
+u= "my dog"    
+M= "sister"
+b=  "my best friend"
+
+print("u and m are playing ") 
+print("I Love u and m")                                       
+print("i love b")
+print("b is 12 years old")
